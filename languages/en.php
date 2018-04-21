41298baf064baa3ffcd2d3a140d676b7dfd1477b
JakimOFF <jakimoff1990@gmail.com>
2018-04-21 12:58:48 +0300

    jakim_portfolio

---
+++ languages/en.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakim
+ * Date: 14.04.2017
+ * Time: 17:22
+ */
+$_['html_lang']                      = 'en';
+$_['home_page']                      = 'http://jakimoff.com';
+$_['select_lang']                    = 'Select Your Language';
+//head
+$_['head_title']                     = '☕ Portfolio By Vitaly Yakimenko';
+//nav
+$_['nav_menu']                       = 'Menu';
+$_['nav_help_menu']                  = 'Toggle navigation';
+$_['nav_logo']                       = 'JakimOFF';
+$_['nav_services']                   = 'Services';
+$_['nav_portfolio']                  = 'Portfolio';
+$_['nav_about']                      = 'About';
+$_['nav_team']                       = 'Team';
+$_['nav_contact']                    = 'Contact';
+//header
+$_['header_intro_in']                = 'Welcome to my page!';
+$_['header_intro']                   = 'It\'s Nice To Meet You';
+$_['header_skills_1']                = 'Web-Developer / HTML5 coder / Bootstrap';
+$_['header_skills_2']                = 'Specialist in cms OpenCart / Joomla / Wordpress';
+$_['header_skills_3']                = 'Programmer PHP / Javascript';
+$_['header_button_1']                = 'what I can do?';
+$_['header_button_2']                = 'What did I do?';
+//services
+$_['services_name']                  = 'Services';
+$_['services_surname']               = 'What can I offer you immediately';
+$_['services_1']                     = 'E-Commerce';
+$_['services_1_desc']                = 'Development of online store based on CMS OpenCart, Joomla, Mod-X. a Complete package necessary for successful trading modules.';
+$_['services_2']                     = 'Development of modules and add-ons including API';
+$_['services_2_desc']                = 'Development of modules customized to the needs of Your business, do the work automated, do not waste your money on the salary.';
+$_['services_3']                     = 'Turnkey website of any complexity';
+$_['services_3_desc']                = 'Develop website from scratch and turnkey. Tell us about your wishes and I will bring them to the web site.';
+$_['services_4']                     = 'The layout of the website + implementation in the CMS';
+$_['services_4_desc']                = 'Is the site design but it does not code? Laid out Your design and put a content management system.';
+$_['services_5']                     = 'Writing scripts in PHP + Mysql + Javascript languages';
+$_['services_5_desc']                = 'If You need the script individually to suit any need, please contact us. Writing scripts in different programming languages.';
+$_['services_6']                     = 'WEB Security';
+$_['services_6_desc']                = 'Your website started long to boot? Do not open the page? See if there are any viruses. Maybe Your website was the victim of hacker attacks.';
+//portfolio
+$_['portfolio_name']                 = 'portfolio';
+$_['portfolio_surname']              = 'The last work performed';
+$_['portfolio_date']                 = 'Date: ';
+$_['portfolio_category']             = 'Category: ';
+$_['portfolio_visited_link']         = 'Visit the Website: ';
+$_['portfolio_closed']               = 'Close Project';
+
+$_['portfolio_project_1_name']       = 'Zoo Room';
+$_['portfolio_project_1_surname']    = 'Improvements on the Joomla website';
+$_['portfolio_project_1_desc']       = 'Conducted refinement modulo VirtualMarket on Joomla cms as the official source spread this module with these flaws, processed correctly pagination of the pages of the catalog of goods made it possible to correctly sort products by producers. As well, this site was moved to https.';
+$_['portfolio_project_1_date']       = 'April 2017';
+$_['portfolio_project_1_category']   = 'Joomla | PHP';
+$_['portfolio_project_1_link']       = '<a href="https://zooroom.com.ua/" target="_blank"> Zoo Room </a>';
+
+$_['portfolio_project_2_name']       = 'Whim Shop';
+$_['portfolio_project_2_surname']    = 'E-shop OpenCart CMS';
+$_['portfolio_project_2_desc']       = 'Added functionality to the boxed version of OpenCart CMS made the ability to display tables of dimensions for all products under each manufacturer separately. Implemented by adding to the admin website appropriate fields to complete the user\'s convenience.';
+$_['portfolio_project_2_date']       = 'April 2017';
+$_['portfolio_project_2_category']   = 'OpenCart | PHP';
+$_['portfolio_project_2_link']       = '<a href="https://whim.dp.ua/" target="_blank"> Whim Shop </a>';
+
+$_['portfolio_project_3_name']       = 'Component for Joomla 2.5';
+$_['portfolio_project_3_surname']    = 'Creating your own component';
+$_['portfolio_project_3_desc']       = 'Developed a component create gift certificates for users of the site, written an image generator of the certificates by application of the required values filled by the user of the website. Implemented through the admin panel as a component.';
+$_['portfolio_project_3_date']       = 'March 2017';
+$_['portfolio_project_3_category']   = 'Joomla | PHP | JS';
+$_['portfolio_project_3_link']       = '<a href="http://m-sport.com.ua/" target="_blank"> M-Sport </a>';
+
+$_['portfolio_project_4_name']       = 'Kazanok Technology';
+$_['portfolio_project_4_surname']    = 'The creation of a site for a blogger';
+$_['portfolio_project_4_desc']       = 'Created website for blogger Dmitry, who is reviewing different gadgets, mobile phones and other electronics. All elements of the site changed through the admin panel of the website. Ease of filling the site with content.';
+$_['portfolio_project_4_date']       = 'March 2016';
+$_['portfolio_project_4_category']   = 'WordPress | PHP | HTML5 | CSS3';
+$_['portfolio_project_4_link']       = '<a href="http://leon.net.ua/" target="_blank"> Kazanok Technology</a>';
+
+$_['portfolio_project_5_name']       = 'Sparkletravel';
+$_['portfolio_project_5_surname']    = 'Website of travel company';
+$_['portfolio_project_5_desc']       = 'Developed the website of tourist company purchased WordPress theme, added the field at the customer\'s request and design type LandingPage. Created blanks of the template pages for the site administrator, to facilitate and optimize the speed of website content.';
+$_['portfolio_project_5_date']       = 'July 2016';
+$_['portfolio_project_5_category']   = 'WordPress | PHP | JS';
+$_['portfolio_project_5_link']       = '<a href="http://sparkletravel.com.ua/" target="_blank"> SparkleTravel </a>';
+
+$_['portfolio_project_6_name']       = 'Improvements on the site';
+$_['portfolio_project_6_surname']    = 'Add functionality to the CMS OpenCart';
+$_['portfolio_project_6_desc']       = 'Feature added in a number of standard on Opencart CMS for the purpose of specification of goods. More specifically added the ability to add a product unit.';
+$_['portfolio_project_6_date']       = 'September 2016';
+$_['portfolio_project_6_category']   = 'OpenCart | PHP';
+$_['portfolio_project_6_link']       = '<a href="https://rhinogroup.com.ua/" target="_blank"> Rhino Group </a>';
+
+//About
+$_['about_name']                     = 'About';
+$_['about_surname']                  = 'Brief information about me';
+
+$_['about_item_1_name']              = 'November 2014';
+$_['about_item_1_surname']           = 'I met the most beautiful girl in the world';
+$_['about_item_1_desc']              = 'There was the one who saw my talent and pushed to start doing programming, because prior to this I worked in the public service';
+
+$_['about_item_2_name']              = '2015-2016';
+$_['about_item_2_surname']           = 'Learn PHP programmer';
+$_['about_item_2_desc']              = 'Having finished your first course in programming, I started working on the exchange of freelancing, as experience in commercial projects not yet exist, the orders were not complicated';
+
+$_['about_item_3_name']              = '2016';
+$_['about_item_3_surname']           = 'He worked in the firm TRICE BIG DATA SOLUTION';
+$_['about_item_3_desc']              = 'Worked as a head of Department in the firm three solutions for big data in San Francisco, California, USA. This is my second job. The first was local firm, where I worked for 3 months.';
+
+$_['about_item_4_name']              = 'December 2016';
+$_['about_item_4_surname']           = 'Finished courses of HTML/CSS';
+$_['about_item_4_desc']              = 'At the end of the year realizing that we need to improve done another course on the layout of sites. Not cease to be interested in new developments in the world of programming';
+
+$_['about_item_5_name']              = 'Be Part<br>Of Our<br>Story!';
+
+//Team
+$_['team_name']                      = 'Our Amazing Team';
+$_['team_surname']                   = 'My trusted friends and colleagues';
+$_['team_text']                      = 'Reliable and friendly team, the key to successful cooperation, my friends know how to do what I am not able, and so we complement each other, which gives the opportunity to develop any complexity.';
+//social media
+$_['team_vk']                        = 'Vkontakte';
+$_['team_li']                        = 'Linkedin';
+$_['team_fb']                        = 'Facebook';
+$_['team_tw']                        = 'Twitter';
+$_['team_viber']                     = 'Viber';
+
+$_['team_item_1_name']               = 'Yakimenko Vita';
+$_['team_item_1_skills']             = 'Accounting, creative Director';
+
+$_['team_item_2_name']               = 'Yakimenko Alexander';
+$_['team_item_2_skills']             = 'Designer, graphics';
+
+$_['team_item_3_name']               = 'Iryna Moyseenko';
+$_['team_item_3_skills']             = 'Naming, slogans, translation';
+
+//Contact us
+$_['contact_name']                   = 'Contact Us';
+$_['contact_surname']                = 'If You have any questions. Ask me!';
---
+++ languages/ua.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakim
+ * Date: 14.04.2017
+ * Time: 17:22
+ */
+$_['html_lang']                      = 'ua';
+$_['home_page']                      = 'http://jakimoff.com';
+$_['select_lang']                    = 'Оберіть мову';
+//head
+$_['head_title']                     = '☕ Портфоліо Якименко Віталія';
+//nav
+$_['nav_menu']                       = 'Меню';
+$_['nav_help_menu']                  = 'Перемикач навігації';
+$_['nav_logo']                       = 'JakimOFF';
+$_['nav_services']                   = 'Послуги';
+$_['nav_portfolio']                  = 'Портфоліо';
+$_['nav_about']                      = 'Про мене';
+$_['nav_team']                       = 'Команда';
+$_['nav_contact']                    = 'Контакти';
+//header
+$_['header_intro_in']                = 'Ласкаво прошу до моєї сторінки!';
+$_['header_intro']                   = 'Радий знайомству';
+$_['header_skills_1']                = 'ВЕБ-розробник / HTML5 верстальщик / Bootstrap';
+$_['header_skills_2']                = 'Спеціаліст в cms OpenCart / Joomla / Wordpress';
+$_['header_skills_3']                = 'Програміст PHP / Javascript';
+$_['header_button_1']                = 'Що я можу зробити?';
+$_['header_button_2']                = 'Що я вже зробив?';
+//services
+$_['services_name']                  = 'Послуги';
+$_['services_surname']               = 'Що я можу Вам запропонувати відразу';
+$_['services_1']                     = 'Електронна комерція';
+$_['services_1_desc']                = 'Розробка інтернет-магазину на основі CMS OpenCart, Joomla, Mod-X. Повний пакет модулів для успішної торгівлі.';
+$_['services_2']                     = 'Розробка модулів та доповненнь, можливо з використанням API';
+$_['services_2_desc']                = 'Розробка модулей з урахуванням індивідуальних потреб Вашого бізнесу. Допоможу зробити все автоматичним.';
+$_['services_3']                     = 'Сайт під ключ будь-якої складності';
+$_['services_3_desc']                = 'Розробка сайта з нуля і повністю під ключ. Скажіть мені про свої бажання і я реалізую їх у вигляду веб-сайту';
+$_['services_4']                     = 'Верстка сайта + інтеграция його в CMS';
+$_['services_4_desc']                = 'Якщо у Вас є дизайн сайта, а потрібно його виконати в вигляді коду, я відверстаю та створю админку сайта для зручного наповнення сайта контентом.';
+$_['services_5']                     = 'Пишу скріпти PHP + Mysql + Javascript мовах';
+$_['services_5_desc']                = 'Якщо Вам потрібний унікальний скріпт щоб задовольнити особливі потреби, напишіть мені. Можлива реалізація самих незвичних задач.';
+$_['services_6']                     = 'WEB Захист';
+$_['services_6_desc']                = 'Ваш сайт почав довго грузитися? Не відкриваються сторінки? Можливо Ваш сайт став жертвою хакерскї атаки. Не чекайте, напишіть і я подивлюся в чім річ.';
+//portfolio
+$_['portfolio_name']                 = 'Портфоліо';
+$_['portfolio_surname']              = 'Останні виконані роботи';
+$_['portfolio_date']                 = 'Дата: ';
+$_['portfolio_category']             = 'Категорія: ';
+$_['portfolio_visited_link']         = 'Відвідати веб сайт: ';
+$_['portfolio_closed']               = 'Закрити проект';
+
+$_['portfolio_project_1_name']       = 'Zoo Room';
+$_['portfolio_project_1_surname']    = 'Покращення сайту на CMS Joomla';
+$_['portfolio_project_1_desc']       = 'Проведені доробки по модулю VirtualMarket на CMS Joomla оскільки офіційне видання даного модуля не выпустило версію де ці помилки виплавлені, поправлена робота пагінації сторінок каталогу товарів, зроблена можливість правильно сортувати продукти по виробнику. Також, даний сайт переведено на протокол https.';
+$_['portfolio_project_1_date']       = 'Квітень 2017';
+$_['portfolio_project_1_category']   = 'Joomla | PHP';
+$_['portfolio_project_1_link']       = '<a href="https://zooroom.com.ua/" target="_blank"> Zoo Room </a>';
+
+$_['portfolio_project_2_name']       = 'Whim Shop';
+$_['portfolio_project_2_surname']    = 'Магазин OpenCart CMS';
+$_['portfolio_project_2_desc']       = 'Добавив функції до стандартної збірки CMS OpenCart, добавлена можливість відображення таблиці розмірів для всіх продуктів по кожному виробнику окремо. Реалізовано шляхом добавлення в адмінку сайту відповідні поля, для повної зручності керування.';
+$_['portfolio_project_2_date']       = 'Квітень 2017';
+$_['portfolio_project_2_category']   = 'OpenCart | PHP';
+$_['portfolio_project_2_link']       = '<a href="https://whim.dp.ua/" target="_blank"> Whim Shop </a>';
+
+$_['portfolio_project_3_name']       = 'Компонент для Joomla 2.5';
+$_['portfolio_project_3_surname']    = 'Створення власного компоненту';
+$_['portfolio_project_3_desc']       = 'Розроблено компонент створення подарункових сертифікатів для користувачів сайту, написаний генератор зображень сертифікатів із вхідних даних від пористувачів. Реалізовано в вигляді компонента із установкою і керуванням через адмінку сайту.';
+$_['portfolio_project_3_date']       = 'Березень 2017';
+$_['portfolio_project_3_category']   = 'Joomla | PHP | JS';
+$_['portfolio_project_3_link']       = '<a href="http://m-sport.com.ua/" target="_blank"> M-Sport </a>';
+
+$_['portfolio_project_4_name']       = 'Kazanok Technology';
+$_['portfolio_project_4_surname']    = 'Створення сайту для блоггера';
+$_['portfolio_project_4_desc']       = 'Створений сайт для блогера Дмитра, який проводить огляди різної електроніки, серед яких гаджети та телефони. Всі елементи сайту змінюються через адмінку сайту, що дає легкість наповнення сайту без знання прогрмування.';
+$_['portfolio_project_4_date']       = 'Березень 2016';
+$_['portfolio_project_4_category']   = 'WordPress | PHP | HTML5 | CSS3';
+$_['portfolio_project_4_link']       = '<a href="http://leon.net.ua/" target="_blank"> Kazanok Technology</a>';
+
+$_['portfolio_project_5_name']       = 'Sparkletravel';
+$_['portfolio_project_5_surname']    = 'Веб сайт туристичної фірми';
+$_['portfolio_project_5_desc']       = 'Розроблений сайт для туристичної фірми на купленній темі під CMS WordPress, перероблений дизайн під замовлення клієнта, створений сайт в вигляді LandingPage. Створені шаблони сторінок для зручного адміністрування сайту та оптимізації швидкості його наповнення.';
+$_['portfolio_project_5_date']       = 'Червень 2016';
+$_['portfolio_project_5_category']   = 'WordPress | PHP | JS';
+$_['portfolio_project_5_link']       = '<a href="http://sparkletravel.com.ua/" target="_blank"> SparkleTravel </a>';
+
+$_['portfolio_project_6_name']       = 'Покращення на сайті';
+$_['portfolio_project_6_surname']    = 'Добавлений функціонал в сайт на CMS OpenCart';
+$_['portfolio_project_6_desc']       = 'Добавка функціонала до стандартного на CMS Opencart з ціллю зручності виводу інформації про товар. Якщо точніше, то добавлена можливість вказувати одиниці виміру конкретного товару.';
+$_['portfolio_project_6_date']       = 'Вересень 2016';
+$_['portfolio_project_6_category']   = 'OpenCart | PHP';
+$_['portfolio_project_6_link']       = '<a href="https://rhinogroup.com.ua/" target="_blank"> Rhino Group </a>';
+
+//About
+$_['about_name']                     = 'Про мене';
+$_['about_surname']                  = 'Коротка інформація про мене';
+
+$_['about_item_1_name']              = 'Листопад 2014';
+$_['about_item_1_surname']           = 'Я зустрів саму красиву дівчину в світі';
+$_['about_item_1_desc']              = 'Вона була той єдиною, і тільки вона розгледіла в мені талант програміста. Вона дала мені впевненість і підтримку, щоб я почав займатися своєю улюбленою справою, оскільки до цього я працював на державу.';
+
+$_['about_item_2_name']              = '2015-2016';
+$_['about_item_2_surname']           = 'Вивчаю PHP програмування';
+$_['about_item_2_desc']              = 'Закінчив свої перші курси програмування, відразу після цього почав працювати на біржі фріланса, оскільки досвіду в великих комерційних проектах ще не було, я брав на виконання не складні замовлення.';
+
+$_['about_item_3_name']              = '2016';
+$_['about_item_3_surname']           = 'Почав працювати на фірмі TRICE BIG DATA SOLUTION';
+$_['about_item_3_desc']              = 'Працював начальником відділу в фірмі із Сан-Франциско, штат Каліфорнія, США. Це моє друге робоче місте, першою була місцева фірма що занепадала де я пропрацював 3 місяці.';
+
+$_['about_item_4_name']              = 'Грудень 2016';
+$_['about_item_4_surname']           = 'Закінчив курси по направленню HTML/CSS';
+$_['about_item_4_desc']              = 'Розуміючи що тільки постійні вивчення чогось нового дадуть добрий результат я закінчив ще одні курси, тепер по верстці сайтів. Не перестаю і по сьогоднішній день цікавитися новинками в сфері IT.';
+
+$_['about_item_5_name']              = 'Станьте<br>частиною Моєї<br>історії!';
+
+//Team
+$_['team_name']                      = 'Моя дивовижна команда';
+$_['team_surname']                   = 'Мої близькі, друзі та коллеги';
+$_['team_text']                      = 'Надійна і дружня команда, ключ для успішної співпраці з крупними фірмами. Мої друзі уміють робити те, що не встиг вивчити я, тому ми доповнюємо одне одного, що дає можливість реазізувати проекти будь-якої складності.';
+//social media
+$_['team_vk']                        = 'Vkontakte';
+$_['team_li']                        = 'Linkedin';
+$_['team_fb']                        = 'Facebook';
+$_['team_tw']                        = 'Twitter';
+$_['team_viber']                     = 'Viber';
+
+$_['team_item_1_name']               = 'Якименко Віта';
+$_['team_item_1_skills']             = 'Бухгалтерский облік, креативний директор';
+
+$_['team_item_2_name']               = 'Якименко Олександр';
+$_['team_item_2_skills']             = 'Дизайнер, графіка';
+
+$_['team_item_3_name']               = 'Ірина Мойсеєнко';
+$_['team_item_3_skills']             = 'Нейминг, слогани, переклад на різні мови';
+
+//Contact us
+$_['contact_name']                   = 'Контакти';
+$_['contact_surname']                = 'Якщо у Вас виникли питання. Запитайте мене!';
